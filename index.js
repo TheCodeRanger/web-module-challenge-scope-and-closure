2d06842e830326409d5e162ffdfbf00a3654762d
Christopher Greene <greenebasecamp@gmail.com>
2020-11-09 22:31:30 -0500

    finished project

--- index.js
+++ index.js
@@ -99,8 +99,11 @@ console.log (finalScore(inning,9));
 // the function should take the inning function as an argument 
 // it should return an object with with a score for home and a score for away that that populates from invoking the inning callback. */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB()
+  }
 }
 /* Task 5: scoreboard()
 Use the scoreboard function below to do the following:
@@ -146,10 +149,23 @@ Use the scoreboard function below to do the following:
   */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreCB, inningCB, num) {
+  const totalGameScore = [];
+  let Home = 0;
+  let Away = 0;
+  for(let i = 0; i < num; i++){
+    Home += getInningScoreCB(inningCB).Home;
+    Away += getInningScoreCB(inningCB).Away;
+    totalGameScore.push(`Inning ${i+1}: Away ${Away} - Home ${Home}`)
+  }
+  if(Home === Away){
+    totalGameScore.push(`This game will require extra innings: Away ${Away} - Home ${Home}`);
+  } else{
+    totalGameScore.push(`Final Score: Away ${Away} - Home ${Home}`);
+  }
+  
 }
-
+ scoreboard(getInningScore, inning, 9);
 
 
 
